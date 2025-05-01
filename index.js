725a25d62bc3be123301fce3c774a2c4d5e9ce95
Zeplin Rowe <rowe_michael101@yahoo.com>
2025-05-01 16:14:25 -0400

    Corrected Froyo

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const froyoOrder = prompt(
   "Enter froyo flavors separated by commas:",
-  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee,coffee"
 );
 const froyoArray = froyoOrder.split(",");
 
-const froyoFlavors = {};
-for (const key in froyoFlavors) {
-  if (froyoFlavors[flavors]) {
-    froyoFlavors[key] = froyoFlavors[key] + 1;
-  }
-}
-froyoFlavors.items = froyoArray;
+froyoFlavors = {};
 
 for (let i = 0; i < froyoArray.length; i++) {
   const flavors = froyoArray[i];
-  console.log(flavors);
+  if (froyoFlavors[flavors]) {
+    froyoFlavors[flavors] += 1;
+  } else {
+    froyoFlavors[flavors] = 1;
+  }
 }
+console.log(froyoFlavors);
